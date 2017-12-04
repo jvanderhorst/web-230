717cd0a73da7fa1a869c1e712f5468b7698dfdf8
Johnny Vanderhorst <johnnyrvanderhorst@gmail.com>
2017-12-03 19:45:59 -0500

    Adding assignment 1.4.js

---
+++ week-1/primitive-types.js
@@ -0,0 +1,45 @@
+// start program
+
+/*
+    Expected output:
+    
+    First name
+    Last name
+    Age
+    Street
+    City
+    State 
+    Zip code 
+*/ 
+
+// variable declaration 
+var firstName;
+var lastName;
+var age;
+var street;
+var city;
+var state;
+var zipCode;
+
+// variable assignment
+
+firstName = "Johnny";
+lastName = "Vanderhorst";
+age = "42";
+street = "1134 Baker St";
+city = "Columbus";
+state = "GA";
+zipCode = "31901";
+
+
+// output 
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(street);
+console.log(city);
+console.log(state);
+console.log(zipCode);
+
+
+// end program
