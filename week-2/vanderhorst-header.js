71e4cb66c2ae3614cbd239d26ad3c271fbf61f0e
Johnny Vanderhorst <johnnyrvanderhorst@gmail.com>
2017-12-10 16:53:27 -0500

    Updated week-2

--- week-2/vanderhorst-header.js
+++ week-2/vanderhorst-header.js
@@ -2,5 +2,5 @@ var header = require('../header.js');
 
 console.log("\nTesting the header.js file's output..."); 
 
-console.log(header.display("Richard", "Krasso", "Exercise 2.2")); 
+console.log(header.display("Johnny", "Vanderhorst", "Exercise 2.2")); 
 console.log("\n");
