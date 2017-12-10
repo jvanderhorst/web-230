99766ff7fe2f6935aedd2b61b45f7d00008814bf
Johnny Vanderhorst <johnnyrvanderhorst@gmail.com>
2017-12-10 11:08:39 -0500

    Adding week-2

---
+++ week-2/vanderhorst-functions.js
@@ -0,0 +1,35 @@
+var header = require('../header.js'); 
+
+// start program
+
+/*
+    Expected output:
+
+    FirstName LastName
+    Exercise 2.3
+    Today's Date
+
+    Hello FirstName LastName!
+
+    Hint: Use your personal header display function and
+    refer to page 178 for implementing function properties
+*/ 
+
+// function properties go here...
+
+
+// function 
+function myName() {
+    return "Johnny Vanderhorst";
+    
+}
+
+// output 
+console.log('\n'); 
+
+
+
+
+
+
+// end program 
---
+++ week-2/vanderhorst-header.js
@@ -0,0 +1,6 @@
+var header = require('../header.js');
+
+console.log("\nTesting the header.js file's output..."); 
+
+console.log(header.display("Richard", "Krasso", "Exercise 2.2")); 
+console.log("\n");
