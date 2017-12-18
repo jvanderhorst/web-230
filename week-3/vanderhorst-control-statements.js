2696bfbd608b9ce285b548c339448ad116ac02b3
Johnny Vanderhorst <johnnyrvanderhorst@gmail.com>
2017-12-17 22:45:36 -0500

    Adding assignment 3.3

---
+++ week-3/vanderhorst-control-statements.js
@@ -0,0 +1,43 @@
+var header = require('../week-2/vanderhorst-header'); 
+
+// start program
+
+/*
+    Expected output:
+
+    FirstName LastName
+    Exercise 3.3
+    Today's Date
+
+    // Expected output
+    The enter key was pressed 
+
+*/ 
+
+console.log('\n');
+console.log(header.display("Johnny", "Vanderhorst", "Exercise 2.4")); 
+console.log('\n');
+
+// Multiway branch of if statements (replace with a switch statement)
+var eventKeyCode = 32;
+
+switch (eventKeyCode) {
+    case 13:
+    console.log("The enter key was pressed"); 
+    break;
+case 16: (eventKeyCode == 16)
+    console.log("The shift key was pressed"); 
+    break;
+ case 32: (eventKeyCode == 32) 
+    console.log("The spacebar key was pressed");
+    break;
+ case 8: (eventKeyCode == 8) 
+    console.log("The backspace / delete key was pressed");
+    break;
+ default:
+    console.log("Unrecognized key press"); 
+    break;
+}
+
+
+// end program 
